85c65a0c16b59b2b3c6c6bef4558f7333d92aebb
Josue J Martinez <josuemartinez678@gmail.com>
2023-11-04 15:56:38 -0500

    Update PS6.java

--- PS6.java
+++ PS6.java
@@ -1,110 +1,109 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PS6 {
-    private int[] restaurants;
+	public static int maxValue;
 
-    public static void main(String[] args) {
-        String inputFile = args[0];
-        int[] restaurants = readInputFromFile(inputFile);
-        PS6 ps6 = new PS6(restaurants);
-        ps6.findMaximumValuePath();
-    }
+	public static void main(String[] args) throws IOException {
+		int[] restaurants = readInputFromFile(args[0]);
+		int[][] table = createMemoTable(restaurants);
+		maxValue = findMaxValue(table);
+		int[] path = findOptimalPath(table);
 
-    public PS6(int[] restaurants) {
-        this.restaurants = restaurants;
-    }
+		System.out.println("Maximum Value: " + maxValue);
+		System.out.println("\nPath");
+		System.out.println("-----------------");
 
-    public void findMaximumValuePath() {
-        int n = restaurants.length;
-        int maxSum = 0;
-        int startRestaurant = -1;
-        int endRestaurant = -1;
+		int subtotal = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int maxValue = calculateMaxValue(i, j, n);
+		for (int i = 0; i < path.length; i++) {
+			int restaurantId = path[i];
+			int value = restaurants[restaurantId];
+			subtotal += value;
+			System.out.println(restaurantId + ": Value = " + value + ", Subtotal: " + subtotal);
+		}
 
-                if (maxValue > maxSum) {
-                    maxSum = maxValue;
-                    startRestaurant = i;
-                    endRestaurant = j;
-                }
-            }
-        }
+	}
 
-        System.out.println("Maximum Value: " + maxSum);
-        System.out.println("Path");
-        System.out.println("-----------------");
+	public static int[][] createMemoTable(int[] restaurants) {
+		int length = restaurants.length;
+		int[][] table = new int[length][length];
 
-        int current = startRestaurant;
-        int subtotal = 0;
-        do {
-            int restaurantIdx = current;
-            int value = restaurants[restaurantIdx];
-            System.out.println(restaurantIdx + ": Value = " + value + ", Subtotal: " + (subtotal + value));
-            subtotal += value;
-            current = (current + 1) % n;
-        } while (current != endRestaurant);
+		// BASE CASE i = 1
+		for (int i = 0; i < length; i++) {
+			table[0][i] = restaurants[i];
+		}
 
-        int restaurantIdx = endRestaurant;
-        int value = restaurants[restaurantIdx];
-        System.out.println(restaurantIdx + ": Value = " + value + ", Subtotal: " + (subtotal + value));
-    }
+		// FILLING TABLE WITH SUBARRAY PROBLEMS
+		for (int i = 1; i < length; i++) {
+			for (int j = 0; j < length; j++) {
+				int above = table[0][j];
+				int diagonal = table[i - 1][(j + 1) % length];
 
-    private int calculateMaxValue(int i, int j, int n) {
-        int maxValue = 0;
-        int sum = 0;
+				table[i][j] = above + diagonal;
 
-        for (int k = i; k <= j; k++) {
-            int value = restaurants[k];
-            sum += value;
-            if (sum > maxValue) {
-                maxValue = sum;
-            }
-            if (sum < 0) {
-                sum = 0;
-            }
-        }
+			}
+		}
 
-        int circularSum = 0;
-        for (int k = (j + 1) % n; k != i; k = (k + 1) % n) {
-            int value = restaurants[k];
-            circularSum += value;
-            if (circularSum > maxValue) {
-                maxValue = circularSum;
-            }
-            if (circularSum < 0) {
-                circularSum = 0;
-            }
-        }
+		return table;
 
-        return maxValue;
-    }
+	}
 
-    private static int[] readInputFromFile(String inputFile) {
-        List<Integer> values = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] parts = line.split(",");
-                if (parts.length == 2) {
-                    int value = Integer.parseInt(parts[1]);
-                    values.add(value);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	public static int[] findOptimalPath(int[][] table) {
 
-        int[] restaurants = new int[values.size()];
-        for (int i = 0; i < values.size(); i++) {
-            restaurants[i] = values.get(i);
-        }
+		// FINDING MAXVALUE AGAIN
+		int maxValue = 0;
+		int endRest = 0;
+		for (int i = 0; i < table.length; i++) {
+			if (maxValue < table[i][table.length - 1]) {
+				maxValue = table[i][table.length - 1];
+				endRest = i;
+			}
+		}
 
-        return restaurants;
-    }
+		// FINDING OPTIMAL PATH
+		int currRestaurant = endRest;
+		int[] path = new int[table.length];
+
+		return path;
+	}
+
+	public static int findMaxValue(int[][] table) {
+		int maxValue = 0;
+		for (int i = 0; i < table.length; i++) {
+			if (maxValue < table[i][table.length - 1]) {
+				maxValue = table[i][table.length - 1];
+			}
+		}
+
+		return maxValue;
+
+	}
+
+	public static int[] readInputFromFile(String inputFile) throws IOException {
+
+		BufferedReader br = new BufferedReader(new FileReader(inputFile));
+		String line;
+		int count = 0;
+		while ((line = br.readLine()) != null) {
+			count++;
+		}
+		br.close();
+
+		int[] restaurants = new int[count];
+		BufferedReader br2 = new BufferedReader(new FileReader(inputFile));
+		String line2;
+		int i = 0;
+		while ((line2 = br2.readLine()) != null) {
+			String[] split = line2.split(",");
+
+			restaurants[i] = Integer.parseInt(split[1]);
+			i++;
+
+		}
+		br2.close();
+		return restaurants;
+
+	}
 }

59e3dd41f4dc27bd6713dbc9e3e0d525cf5a4587
Josue J Martinez <josuemartinez678@gmail.com>
2023-11-28 22:38:33 -0600

    Update PS9.java & Vertex.java

--- PS9.java & Vertex.java
+++ PS9.java & Vertex.java
@@ -5,11 +5,9 @@ import java.util.*;
 
 public class PS9WeightedGraph {
 	
-	public static char[][] matrix;
-	public static int numRows;
-	public static int numCols;
-	
 	public static void main(String[] args) {
+		
+		
 		
 		if (args.length != 3) {
             System.out.println("Invalid command syntax.");
@@ -22,10 +20,17 @@ public class PS9WeightedGraph {
         char destinationVertex = args[2].charAt(0);
 
         Graph graph = new Graph();
+        
 
         try {
             buildGraph(graph, inputFile);
-
+            graph.printWeights();
+            Vertex source = graph.getVertexByName(Character.toString(sourceVertex));
+            Vertex destination = graph.getVertexByName(Character.toString(destinationVertex));
+            
+            graph.setSourceVertex(source);
+            graph.setDestinationVertex(destination);
+            
             System.out.println("===== Graph G Information =====");
             System.out.print("Vertices: ");
             for (Vertex vertex : graph.getVertices()) {
@@ -36,7 +41,7 @@ public class PS9WeightedGraph {
             System.out.println("# of edges: " + graph.getEdgesCount());
 
             System.out.print("Unreachable Vertices: ");
-            List<String> unreachableVertices = findUnreachableVertices(graph, graph.getVertexByChar(destinationVertex));
+            List<String> unreachableVertices = findUnreachableVertices(graph, graph.getDestinationVertex());
             if (unreachableVertices.isEmpty()) {
                 System.out.println("<none>");
             } else {
@@ -57,74 +62,74 @@ public class PS9WeightedGraph {
 	}
 	
 	public static void buildGraph(Graph G, String inputFile) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(inputFile));
-        String line;
+	    BufferedReader br = new BufferedReader(new FileReader(inputFile));
+	    String line;
 
-        while ((line = br.readLine()) != null) {
-            String[] parts = line.split(",");
-            if (parts.length == 3) {
-                String source = parts[0];
-                String destination = parts[1];
-                int weight = Integer.parseInt(parts[2]);
+	    while ((line = br.readLine()) != null) {
+	        String[] parts = line.split(",");
+	        if (parts.length == 3) {
+	            String source = parts[0];
+	            String destination = parts[1];
+	            int weight = Integer.parseInt(parts[2]);
 
-                Vertex sourceVertex = G.getSourceVertex();
-                Vertex destVertex = G.getDestinationVertex();
+	            Vertex sourceVertex = G.getVertexByName(source);
+	            if (sourceVertex == null) {
+	                sourceVertex = new Vertex(source);
+	                G.addVertex(sourceVertex);
+	            }
 
-                if (sourceVertex == null) {
-                    sourceVertex = new Vertex(source);
-                    G.addVertex(sourceVertex);
-                }
+	            Vertex destinationVertex = G.getVertexByName(destination);
+	            if (destinationVertex == null) {
+	                destinationVertex = new Vertex(destination);
+	                G.addVertex(destinationVertex);
+	            }
 
-                if (destVertex == null) {
-                    destVertex = new Vertex(destination);
-                    G.addVertex(destVertex);
-                }
+	            G.addEdge(sourceVertex, destinationVertex, weight);
+	        }
+	    }
 
-                G.addEdge(sourceVertex, destVertex, weight);
-            }
-        }
-
-        br.close();
-    }
+	    br.close();
+	}
 	
 	
 	public static void runDijkstra(Graph g, Vertex s) {
-        System.out.printf("[Step 2]:  Running Dijkstra's algorithm.%n");
+	    System.out.printf("[Step 2]:  Running Dijkstra's algorithm.%n");
 
-        for (Vertex u : g.getVertices()) {
-            u.setD(Integer.MAX_VALUE);
-            u.setPi(null);
-        }
+	    for (Vertex u : g.getVertices()) {
+	        u.setD(Integer.MAX_VALUE);
+	        u.setPi(null);
+	    }
 
-        s.setD(0);
+	    s.setD(0);
 
-        PriorityQueue<Vertex> minHeap = new PriorityQueue<>(Comparator.comparingInt(Vertex::getD));
-        minHeap.add(s);
+	    PriorityQueue<Vertex> minHeap = new PriorityQueue<>(Comparator.comparingInt(Vertex::getD));
+	    minHeap.add(s);
 
-        while (!minHeap.isEmpty()) {
-            Vertex u = minHeap.poll();
+	    while (!minHeap.isEmpty()) {
+	        Vertex u = minHeap.poll();
 
-            for (Vertex v : g.getAdjacencyList(u)) {
-                relax(u, v, g, minHeap);
-            }
-        }
+	        for (Vertex v : g.getAdjacencyList(u)) {
+	            relax(u, v, g, minHeap);
+	        }
+	    }
 
-        System.out.printf("[Step 2]:  Dijkstra's algorithm complete.%n");
-    }
+	    System.out.printf("[Step 2]:  Dijkstra's algorithm complete.%n");
 
-    private static void relax(Vertex u, Vertex v, Graph g, PriorityQueue<Vertex> minHeap) {
-        int weightUV = g.getWeight(u, v);
-        int distanceThroughU = u.getD() + weightUV;
+	}
 
-        if (distanceThroughU < v.getD()) {
-            minHeap.remove(v);
-            v.setD(distanceThroughU);
-            v.setPi(u);
-            minHeap.add(v);
-        }
-    }
+	private static void relax(Vertex u, Vertex v, Graph g, PriorityQueue<Vertex> minHeap) {
+	    int weightUV = g.getWeight(u, v);
+	    int distanceThroughU = u.getD() + weightUV;
 
-    private static List<String> printPath(Vertex source, Vertex target, String algorithm) {
+	    if (u.getD() != Integer.MAX_VALUE && distanceThroughU < v.getD()) {
+	        minHeap.remove(v);
+	        v.setD(distanceThroughU);
+	        v.setPi(u);
+	        minHeap.add(v);
+	    }
+	}
+
+    private static List<String> printPath(Vertex source, Vertex target) {
         List<String> coordinates = new ArrayList<>();
         Vertex current = target;
 
@@ -135,11 +140,69 @@ public class PS9WeightedGraph {
 
         return coordinates;
     }
+    
+    
+    public static List<Integer> findGraphMaxTolls(Graph G, char sourceVertex) {
+        List<Integer> maxTolls = new ArrayList<>();
+        Vertex source = G.getSourceVertex();
+
+        if (source != null) {
+            runDijkstra(G, source);
+
+            for (Vertex destination : G.getVertices()) {
+                if (destination != null && destination != source) {
+                    List<String> path = printPath(source, destination);
+                    int maxToll = findMaxTollAlongPath(G, path);
+                    maxTolls.add(maxToll);
+                }
+            }
+        }
+        
+        return maxTolls;
+    }
+
+    public static int findSmallestMaxToll(Graph G, char sourceVertex, char destinationVertex) {
+        Vertex source = G.getSourceVertex();
+        Vertex destination = G.getDestinationVertex();
+
+        if (source != null && destination != null) {
+            runDijkstra(G, source);
+            List<String> path = printPath(source, destination);
+            return findMaxTollAlongPath(G, path);
+        }
+
+        return Integer.MAX_VALUE;
+    }
+
+    private static int findMaxTollAlongPath(Graph G, List<String> path) {
+        int maxToll = Integer.MIN_VALUE;
+
+        for (int i = 0; i < path.size() - 1; i++) {
+            Vertex u = G.getVertexByName(path.get(i));
+            Vertex v = G.getVertexByName(path.get(i + 1));
+
+            int weightUV = G.getWeight(u, v);
+            maxToll = Math.max(maxToll, weightUV);
+        }
+
+        return maxToll;
+    }
+
+    public static List<String> findUnreachableVertices(Graph G, Vertex destination) {
+        List<String> unreachableVertices = new ArrayList<>();
+
+        for (Vertex vertex : G.getVertices()) {
+            if (!vertex.equals(destination) && vertex.getD() == Integer.MAX_VALUE) {
+                unreachableVertices.add(vertex.getName());
+            }
+        }
+
+        return unreachableVertices;
+    }
 	
 	
 	
-	
-	class Graph {
+	static class Graph {
 
 		private HashSet<Vertex> vertices;
 		private HashMap<Vertex, List<Vertex>> adjList;
@@ -181,16 +244,26 @@ public class PS9WeightedGraph {
 		 * 
 		 */
 		public void addEdge(Vertex src, Vertex dest, int weight) {
-            List<Vertex> srcNeighbors = adjList.get(src);
+		    List<Vertex> srcNeighbors = adjList.get(src);
+		    List<Vertex> destNeighbors = adjList.get(dest);
 
-            if (srcNeighbors == null) {
-                srcNeighbors = new ArrayList<>();
-                adjList.put(src, srcNeighbors);
-            }
+		    if (srcNeighbors == null) {
+		        srcNeighbors = new ArrayList<>();
+		        adjList.put(src, srcNeighbors);
+		    }
 
-            srcNeighbors.add(dest);
-            dest.setWeight(src, weight);  // Assuming you have a setWeight method in your Vertex class.
-        }
+		    if (destNeighbors == null) {
+		        destNeighbors = new ArrayList<>();
+		        adjList.put(dest, destNeighbors);
+		    }
+
+		    srcNeighbors.add(dest);
+		    destNeighbors.add(src);
+
+		    src.setWeight(dest, weight);
+		    dest.setWeight(src, weight);
+		}
+
 
 		/**
 		 * This method will remove the edge from vertex src to destination vertex dest.
@@ -224,7 +297,7 @@ public class PS9WeightedGraph {
 		 * This method will set the destination vertex
 		 */
 		public void setDestinationVertex(Vertex u) {
-			this.source = u;
+			this.destination = u;
 		}
 
 		/**
@@ -252,10 +325,15 @@ public class PS9WeightedGraph {
 			return this.vertices;
 		}
 
-		public Vertex getVertexByCoordinates(int row, int col) {
-			String vertexName = row + "_" + col;
-			return vertexMap.get(vertexName);
-		}
+		public Vertex getVertexByName(String name) {
+	        for (Vertex vertex : vertices) {
+	            if (vertex.getName().equals(name)) {
+	                return vertex;
+	            }
+	        }
+
+	        return null;
+	    }
 
 		public HashMap<String, Vertex> getVertexMap() {
 			return this.vertexMap;
@@ -266,13 +344,31 @@ public class PS9WeightedGraph {
 	        return u.getWeight(v);
 	    }
 	
-	
+		public int getEdgesCount() {
+	        int count = 0;
+
+	        for (List<Vertex> neighbors : adjList.values()) {
+	            count += neighbors.size();
+	        }
+
+	        return count;
+	    }
+		
+		public void printWeights() {
+		    for (Vertex u : vertices) {
+		        for (Vertex v : getAdjacencyList(u)) {
+		            int weight = getWeight(u, v);
+		            System.out.println("Edge: " + u.getName() + " -> " + v.getName() + ", Weight: " + weight);
+		        }
+		    }
+		}
 	
 	
 	
 	}
 
 }
+
 
 
 
